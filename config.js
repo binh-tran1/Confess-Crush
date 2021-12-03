6cea2d77add22798b0facd763d93674d7a3a547d
binh-tran1 <95468952+binh-tran1@users.noreply.github.com>
2021-12-03 15:12:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi Trần Ngọc Nhi",
+    introTitle: 'Trần Ngọc Nhi',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -10,11 +10,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    question: 'Trên thế giới hơn 8 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho Bình <3',
+    reply: 'Yêu thì yêu mà không yêu thì cũng yêu <3333333333333333',
+    mess: 'Bìnhh biết mà híii híii 🥰. Bìnhh cũmm yêu Nhii nhiều nhiều 😘😘',
+    messDesc: 'Mốtttt gặpp béee chơmm anhh nhooo.',
+    btnAccept: 'Óoo kayy nunnn <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
